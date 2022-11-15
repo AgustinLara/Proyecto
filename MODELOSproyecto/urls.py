085ba879ec1f8bd1d6fd1cf386b4b66b11e65ba4
AgsutinLara <agustinx123@gmail.com>
2022-11-15 18:15:41 -0300

    paths

---
+++ MODELOSproyecto/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+urlpatterns =[
+    path("Inicio/", Inicio),
+    path("Items/", Items),
+    path("Buffs_Nerfeos", Buffs_Nerfeos),
+]
--- MODELOSproyecto/views.py
+++ MODELOSproyecto/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from MODELOSproyecto.models import Infrmacion 
 
+def Inicio(request):
+    return HttpResponse("Bienvenidos a ¡LOB!")
+
+def Items(request):
+    return HttpResponse("Seccion de ITEMS")
+
+def Buffs_Nerfeos(request):
+    return HttpResponse("Nerfeos y BUffeos de la UBDATE")
 
